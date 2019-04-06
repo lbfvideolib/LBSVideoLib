8a81975aee8754de894edf4493e1fc31ae6a3573
jainnitin2411 <jainnitin2411@gmail.com>
2019-04-07 00:12:24 +0530

    Push template form changes

---
+++ LBFVideoLib.Admin/AdminHelper.cs
@@ -0,0 +1,43 @@
+﻿using LBFVideoLib.Common;
+using LBFVideoLib.Common.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LBFVideoLib.Admin
+{
+    public class AdminHelper
+    {
+
+        public static string GenerateNewMemoNumber()
+        {
+            // Decrypt file
+            int counter = 1;
+
+            try
+            {
+                if (System.IO.File.Exists(ClientHelper.GetMemoNumberFilePath()))
+                {
+                    counter = Cryptograph.DecryptObject<int>(ClientHelper.GetMemoNumberFilePath());
+
+                    // Increment memo number and store it
+                    counter += 1;
+                    // Encrypt file with new memo number.
+                    System.IO.File.Delete(ClientHelper.GetMemoNumberFilePath());
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.HandleException(ex, "", false);
+            }
+            finally
+            {
+                Cryptograph.EncryptObject(counter, ClientHelper.GetMemoNumberFilePath());
+            }
+            return counter.ToString();
+        }
+
+     
+    }
+}
